af7009092cd5ae1cf2f256ef1c0f2e139790f4b7
Oleksii Frolov <oleksii.frolov@lge.com>
2015-04-02 10:24:03 -0700

    Change scene service to foreground

--- app/src/main/java/com/connectsdk/smarthomesampler/SceneService.java
+++ app/src/main/java/com/connectsdk/smarthomesampler/SceneService.java
@@ -19,12 +19,15 @@
  */
 package com.connectsdk.smarthomesampler;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.PowerManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.connectsdk.smarthomesampler.scene.SceneController;
@@ -32,9 +35,12 @@ import com.connectsdk.smarthomesampler.scene.SceneController;
 
 public class SceneService extends Service {
 
+    private static final int NOTIFICATION_ID = 1;
+
     private final IBinder mBinder = new LocalBinder();
 
     private SceneController controller;
+
     private PowerManager.WakeLock wakeLock;
 
     public void createController(IUserNotificationListener listener) {
@@ -81,13 +87,11 @@ public class SceneService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        Log.d("", "sceneservice bind");
         return mBinder;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        Log.d("", "sceneservice unbind");
         return super.onUnbind(intent);
     }
 
@@ -99,21 +103,29 @@ public class SceneService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("", "scene service created");
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Wake Lock");
         wakeLock.acquire();
-        Log.d("", "sceneservice create");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.smarthome_service_is_working))
+                .setContentIntent(pendingIntent).build();
+
+        startForeground(NOTIFICATION_ID, notification);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        stopForeground(true);
         wakeLock.release();
-        Log.d("", "scene service destroyed");
         if (controller != null) {
             controller.disconnect();
         }
-        Log.d("", "sceneservice destroy");
     }
 }
